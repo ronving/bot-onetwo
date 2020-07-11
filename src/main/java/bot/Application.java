6e3afcee83e0c7bfa11223131d98471a75058cfa
Mykhailo Olkhovyi <mykhailo_olkhovyi@epam.com>
2020-07-11 20:01:31 +0300

    fix errors with fing webelement. new max timeout 30s

--- src/main/java/bot/Application.java
+++ src/main/java/bot/Application.java
@@ -1,14 +1,17 @@
 package bot;
 
-import org.omg.PortableServer.THREAD_POLICY_ID;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 import static bot.Keys.*;
 
@@ -17,22 +20,39 @@ public class Application {
     private static final String pathToElement = "/html/body/div/div[2]/main/div/div/div/div[3]/span/div/div[1]/div/div[2]/div[1]";
     private static WebElement playersOnServer;
 
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", pathToDriver);
 
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
         WebDriver webDriver = new ChromeDriver();
+
         webDriver.manage().window().maximize();
         webDriver.get(WEBSITE);
 
         WebDriverWait webDriverWait = new WebDriverWait(webDriver, 15);
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(pathToElement)));
 
-        do {
-            webDriver.navigate().refresh();
-            webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(pathToElement)));
-            playersOnServer = webDriver.findElement(By.xpath(pathToElement));
-            System.out.println(playersOnServer.getText().charAt(0));
-        } while (playersOnServer.getText().charAt(0) < '3');
+        while (true) {
+            if (playersOnServer != null) {
+                webDriver.navigate().refresh();
+                Thread.sleep(15 * 1000);
+            }
+
+            try {
+                webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(pathToElement)));
+                playersOnServer = webDriver.findElement(By.xpath(pathToElement));
+            } catch (Exception e) {
+                webDriver.navigate().refresh();
+                continue;
+            }
+
+
+            if (playersOnServer.isDisplayed()) {
+                Character players = playersOnServer.getText().charAt(0);
+                System.out.println(players);
+                if (players > '2') {
+                    break;
+                }
+            }
+        }
 
         MailSender.sendMail(MAIL_RECEIVER, playersOnServer.getText().charAt(0));
 
